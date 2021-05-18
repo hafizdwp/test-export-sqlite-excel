bb2313e7b4973f05be983aa37bc8753baaa61910
hafizdwp <hafizdwp@gmail.com>
2021-05-18 17:39:22 +0700

    add export permission

--- app/src/main/java/com/hafizdwp/test_export_sqlite_excel/MainActivity.kt
+++ app/src/main/java/com/hafizdwp/test_export_sqlite_excel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hafizdwp.test_export_sqlite_excel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ajts.androidmads.library.SQLiteToExcel
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,20 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.start()
         viewModel.print()
+
+        val export = SQLiteToExcel(this, "database")
+        export.exportAllTables("tesexport.xls", object : SQLiteToExcel.ExportListener {
+            override fun onStart() {
+                log("exporting...")
+            }
+
+            override fun onCompleted(filePath: String?) {
+                log("export complete!")
+            }
+
+            override fun onError(e: Exception?) {
+                logError("export error: $e")
+            }
+        })
     }
 }
